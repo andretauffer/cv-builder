7bc306ac7f6020985e0f2538a21b3827e9fb9529
André Tauffer <andre@iotcomms.io>
2022-09-29 22:27:34 +0200

    Added linting and readme

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
-# cv
+# CV-builder
+
+## Goal
+To create an open source package with features that make it easier for developers to have a nice looking interactive CV, to launch it as a website, and to be able to print it to PDF and distribute it. 
+
+The package should take a json object with the content that matches a json-schema. Since it's json-schema based, it should be easy to later on generate a form that allows users to generate CVs online and print them. 
+
+There will be a basic styling, and the possibility to customize using CSS selectors. More customization capabilities can be added later on, but it's not the initial priority.
+
+## Purpose
+This project is meant to make it simple and easy for developers to build a good looking CV.
+This is a project to exercise and test some techniques and frameworks, like: json-schemas, playwright, react state managing.
+
+json-schemas: explore it as a way of keeping a documented and validated format for the input of the module, so that it's easier to document while increasing the functionality, since documentation becomes part of the code.
+
+playwright: because it seems faster than cypress.
+
+react state managing: reflect upon the state managing pattern, exercising the critic view of when a state should be local and when should it be shared in a context. 
+
+code pattern: exercise on the code pattern of having a function with multiple paths depending on argument and configuration. So far I like the readability of it.
+
+front end magic: also wanna try some cool effects to add to my frontend toolbelt.
+
+## MVP
+
+- Parse a json and generate a CV.
+- Read images specified in the public directory
+- Allow for the following sections:
+  - intro
+  - experiences
+  - technologies
+  - education
+  - projects
+- Fully tested
+  - unit tests full coverage
+  - snapshot tests
+  - play with playwright to test it
+
+## Tech stack
+- React
+- Vite
+- Json-Schema
--- src/components/utils.js
+++ src/components/utils.js
@@ -2,45 +2,45 @@
 const months = ["jan", "feb", "mar", "apr", "may", "jun", "jul", "aug", "sep", "oct", "nov", "dev"];
 
 export const parseDate = ({ date }) => {
-  if (date.toLowerCase() === "current") {
-    return date;
-  } else if (date) {
-    const dateObj = new Date(date);
+	if (date.toLowerCase() === "current") {
+		return date;
+	} else if (date) {
+		const dateObj = new Date(date);
 
-    const day = dateObj.getDate();
-    const month = months[dateObj.getMonth()];
-    const year = dateObj.getFullYear();
-    return `${day} ${month} ${year}`
-  }
+		const day = dateObj.getDate();
+		const month = months[dateObj.getMonth()];
+		const year = dateObj.getFullYear();
+		return `${day} ${month} ${year}`;
+	}
 };
 
 export const parseDescription = ({ description }) => description
-  .split(/\.\s/)
-  .filter(e => !!e)
-  .map(string => !string.endsWith(".") ? string + ". " : string);
+	.split(/\.\s/)
+	.filter(e => !!e)
+	.map(string => !string.endsWith(".") ? string + ". " : string);
 
 export const parseFilterTerms = ({ filter }) => filter.split(/,/g).filter(e => !!e.trim()).map(term => term.trim());
 
 export const filterTechnologies = ({ technologies, filter, keywords }) => {
 
-  let filterTerms = [];
+	let filterTerms = [];
 
-  if (filter) filterTerms = parseFilterTerms({ filter });
+	if (filter) filterTerms = parseFilterTerms({ filter });
 
-  if (keywords && keywords.length) filterTerms = [...filterTerms, ...keywords];
+	if (keywords && keywords.length) filterTerms = [...filterTerms, ...keywords];
 
-  if (!filterTerms.length) return technologies;
+	if (!filterTerms.length) return technologies;
 
-  return technologies
-    .filter(({ title, keywords }) => {
+	return technologies
+		.filter(({ title, keywords }) => {
 
-      let matchingTerms = [title];
-      if (keywords && keywords.length) matchingTerms = [...matchingTerms, ...keywords]
+			let matchingTerms = [title];
+			if (keywords && keywords.length) matchingTerms = [...matchingTerms, ...keywords];
 
-      return filterTerms.reduce((prev, curr) => {
-        const match = matchingTerms.reduce((mprev, mcurr) => mcurr.toLowerCase() === curr.toLowerCase() ? true : mprev, false);
+			return filterTerms.reduce((prev, curr) => {
+				const match = matchingTerms.reduce((mprev, mcurr) => mcurr.toLowerCase() === curr.toLowerCase() ? true : mprev, false);
 
-        return match ? true : prev;
-      }, false);
-    });
+				return match ? true : prev;
+			}, false);
+		});
 };
