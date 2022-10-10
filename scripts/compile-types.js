e63188c7e105b6aa2851a382054ae617191e1f48
André Tauffer <andre@iotcomms.io>
2022-10-10 18:57:44 +0200

    Deploy branch

--- scripts/compile-types.js
+++
@@ -1,9 +0,0 @@
-import { compileFromFile } from 'json-schema-to-typescript'
-import fs from "fs";
-
-// compile from file
-compileFromFile('src/schemas/projects-schema.json')
-  .then(ts => fs.writeFileSync('src/types/projects.d.ts', ts))
-
-compileFromFile('src/schemas/content-schema.json')
-  .then(ts => fs.writeFileSync('src/types/content.d.ts', ts))
--- src/ViewConfigurations.jsx
+++
@@ -1,3 +0,0 @@
-export const breakPoint1 = "1200px";
-
-export const breakPoint2 = "700px";
